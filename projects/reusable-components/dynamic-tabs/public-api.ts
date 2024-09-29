f13d3397f419886716f718982e60271ac521f3ad
tfs@realsoft-me.com <kawthar_alkhouli@outlook.com>
2024-09-29 12:51:10 +0300

    remove dynamic tabs control

--- projects/reusable-components/dynamic-tabs/public-api.ts
+++
@@ -1,6 +0,0 @@
-
-export * from "./src/tab-wrapper/tab-wrapper.module";
-export * from "./src/tab-wrapper/tab-wrapper.component";
-export * from "./src/tab/tab.component";
-export * from "./src/tab-content/tab-content.component";
-export * from "./src/tab-label/tab-label.component";
