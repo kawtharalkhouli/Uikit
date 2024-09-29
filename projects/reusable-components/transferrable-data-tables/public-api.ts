ed62aecd490f6abeb088a4bbfa1c796849ed2a1a
tfs@realsoft-me.com <kawthar_alkhouli@outlook.com>
2024-09-29 13:18:17 +0300

    remove the transferrable data tables control

--- projects/reusable-components/transferrable-data-tables/public-api.ts
+++
@@ -1,5 +0,0 @@
-export * from "./src/transferrable-data-tables.module";
-export * from "./src/transferrable-data-tables.component";
-export * from "./src/helpers/models";
-export * from "./src/helpers/CustomMatPaginatorIntl.class";
-export * from "./src/services/table.service";
